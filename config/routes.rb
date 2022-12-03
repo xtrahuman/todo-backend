98abb72e5ed5370c7a272a1d82cfff9d6b83b4eb
xtrahuman <okpara_tochukwu@yahoo.com>
2022-12-03 06:31:47 +0100

    setup database routes and migration

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :categories do
+    resources :tasks
+  end
 end
--- db/migrate/20221202212221_create_tasks.rb
+++ db/migrate/20221202212221_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[7.0]
     create_table :tasks do |t|
       t.string :name
       t.string :description
-      t.boolean :completed
+      t.boolean :completed, null: false, default: false
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
