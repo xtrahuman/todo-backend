f34e622985a0c8f816bb9cc387858e5243f45d43
xtrahuman <okpara_tochukwu@yahoo.com>
2022-12-06 12:20:57 +0100

    remover order in show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
     end
 
     def show
-        @category = Category.find_by(id: params[:id]).order('created_at DESC')
+        @category = Category.find_by(id: params[:id])
         render json:@category, status: :ok
     end
 
